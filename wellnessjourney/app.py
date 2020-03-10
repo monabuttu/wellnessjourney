7ccc2e2c7570da9cf411ac509ff65dc4df9bb6ea
monabuttu <53659505+monabuttu@users.noreply.github.com>
2020-03-10 18:11:59 -0400

    updating mongodb

--- wellnessjourney/app.py
+++ wellnessjourney/app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 # create instance of flask class
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/quotes")
+mongo = PyMongo(app, uri="mongodb://heroku_5dsnbzq6:n7n5lhm2511elp2vi6ennpsqh5@ds149998.mlab.com:49998/heroku_5dsnbzq6")
 
 @app.route("/")
 def home():
