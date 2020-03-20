a4724ec3350597d5295bed98f8fcc5babcaf9a3b
monabuttu <53659505+monabuttu@users.noreply.github.com>
2020-03-20 19:14:12 -0400

    updated graph

--- wellnessjourney/static/js/graphs.js
+++ wellnessjourney/static/js/graphs.js
@@ -1,11 +1,20 @@
 
+(function() {
+
+var c1 = d3.select('#chart')
+
 d3.json('/api/yourentries').then(entries=>{
     console.log(entries[0]);
-
-    var parseTime = d3.timeParse("%B %d, %Y")
-    parseTime(entries[0].date);
+    var formatTime = d3.timeFormat("%B %d, %Y")
 
     var dates = entries.map(d => d.date);
+    var new_dates = [];
+    dates.forEach(d=> {
+        // console.log(d);
+        x = formatTime(new Date(d))
+        new_dates.push(x);
+        });
+    
     var classification = entries.map(d=> d.post);
     // Part 3 - Line Chart
     var trace1 = {
@@ -17,21 +26,60 @@ d3.json('/api/yourentries').then(entries=>{
     var data = [trace1];
 
     var layout = {
-        autosize: false,
-        width: 500,
-        height: 500,
+        autosize: true,
         margin: {
-        // l: 50,
-        r: 50,
-        b: 100,
-        t: 100,
-        pad: 4
+            "b":95,
+            "l":65,
+            "r":25,
+            "t":30
         }
 
     };
 
-    Plotly.newPlot("chart", data, layout);
+    Plotly.newPlot("chart", data, layout, {staticPlot: true});
 
 });
 
+window.onresize = function() {
+    Plotly.Plots.resize(c1);
+    
+d3.json('/api/yourentries').then(entries=>{
+    console.log(entries[0]);
+    var formatTime = d3.timeFormat("%B %d, %Y")
 
+    var dates = entries.map(d => d.date);
+    var new_dates = [];
+    dates.forEach(d=> {
+        // console.log(d);
+        x = formatTime(new Date(d))
+        new_dates.push(x);
+        });
+    
+    var classification = entries.map(d=> d.post);
+    // Part 3 - Line Chart
+    var trace1 = {
+        x: dates,
+        y: classification,
+        type: "bar"
+    };
+
+    var data = [trace1];
+
+    var layout = {
+        autosize: true,
+        margin: {
+            "b":95,
+            "l":65,
+            "r":25,
+            "t":30
+        }
+
+    };
+
+    Plotly.newPlot("chart", data, layout, {staticPlot: true});
+
+});
+
+ };
+
+})();
